6bdb04fe91c88fab82907c1447a6eef0bce5c384
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2019-11-13 20:11:20 -0600

    Initial Commit

---
+++ Lab6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab6
+//
+//  Created by Tag1 on 11/13/19.
+//  Copyright © 2019 Lab 6. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
